972ea7db76df7c6cbe48f31b8b1b45dff04f840d
Gheorghe Morari <gmorari404@gmail.com>
2019-11-06 23:56:48 +0200

    Mostly working but has troubles with minuses.

--- Source.c
+++ Source.c
@@ -3,7 +3,7 @@
 #include "input.h"
 #include "operations.h"
 #define maxOperations 255
-#define debug 0
+#define debug 1
 // float version branch from scratch
 
 struct operation {
@@ -49,13 +49,13 @@ int setOrder(int start, int end, int numb) {   //a recursive function to set the
 	int j = 0;
 	int number = numb;        //number is the current index of operation, used for recursion
 	for (i = start; i < end; i++)
-		if (arr[i] == '(') {
+		if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{') {
 			number = setOrder(i + 1, getEnd(i), number);
 			i = getEnd(i);
 		}
 
 	for (i = start; i < end; i++) {
-		if (arr[i] == '(') {
+		if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{') {
 			i = getEnd(i); //skip brackets
 		}
 		if (arr[i] == '/' || arr[i] == '*') {  // check if operation is / or *
@@ -65,19 +65,20 @@ int setOrder(int start, int end, int numb) {   //a recursive function to set the
 	}
 
 	for (i = start; i < end; i++) {
-		if (arr[i] == '(') {
+		if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{') {
 			i = getEnd(i); //skip brackets
 		}
 		if (arr[i] == '-' || arr[i] == '+') {  // check if operation is + or -
 			int isOperation = 1;
 			if (arr[i] == '-') {
-				for (j = i-1; j > 0; j--) {
+				for (j = i - 1; j >= -1; j--) {
 					if (isNumber(j) == 1) break;
 					if (isNumber(j) == -2 || isNumber == -1) {
-						isOperation = 0;
+						//isOperation = 0;
+						break;
 					}
 				}
-			} 
+			}
 			if (isOperation == 1) {
 				operations[number].position = i;
 				number++;
@@ -87,85 +88,56 @@ int setOrder(int start, int end, int numb) {   //a recursive function to set the
 	return number;
 }
 
-int calculate(int index, int num) { //this function calculates the result of the operation
+float calculate(int index, int num) { //this function calculates the result of the operation
 	int i, j = 0, trig = 0;
 	float temp = 0, left = 0, right = 0, leftminus = 1, rightminus = 1;
 	for (i = index; i >= 0; i--) //find the left side number
-		if (isNumber(i) == 1) break;
-		else if (isNumber(i) == -1) {
-			operations[num].leftNumber = 0;
+		if (validposition[i] == 1 && isNumber != -1) {
+			operations[num].leftNumber = mainarr[i];
+			operations[num].leftEdge = i;
+			trig = 1;
 			break;
 		}
-	j = 0;
-	while (isNumber(i) == 1) { //save the left number as an int in the struct
-		int c = arr[i] - '0';
-		left += c * tenToPower(j);
-		j++;
-		i--;
+	if (trig == 0) {
+		operations[num].leftEdge = 0;
+		operations[num].leftNumber = 0;
 	}
-	if (isNumber(i) != -1) {
-		if (arr[i] == '-' && checkIfNotOperation(i,num) == 1) //check if left number is negative
-			leftminus = -1;
-	}
-
-	operations[num].leftEdge = i;
-	operations[num].leftNumber = left*leftminus;
-	operations[num].leftminus = leftminus;
-
+	trig = 0;
 	for (i = index; i < length; i++) { //find the right side number
-		if (isNumber(i) != -1 && checkIfNotOperation(i,num) == 1 && isNumber(i+1) == 1) {
-			if (arr[i] == '-' )
-				rightminus = -1; //check if right number is negative
-		}
-		if (isNumber(i) == 1) break;
-		else if (isNumber(i) == -1) { // if length has been exceded then set right to 0
-			operations[num].rightNumber = 0;
+		if (validposition[i] == 1 && isNumber != -1) {
+			operations[num].rightNumber = mainarr[i];
+			operations[num].rightEdge = i;
+			trig = 1;
+			break;
 		}
 	}
-	j = 0;
-	while (isNumber(i) == 1) { // get the length of the number while tracking forward
-		j++;
-		i++;
-	} 
-	int t = i;
-	for (i = j; i > 0; i--){
-		int c = arr[t+i-j-1] - '0';
-		int pow = tenToPower(j - i); // set the right number while tracking backwards
-			right += c * pow;
+	if (trig == 0) {
+		operations[num].rightEdge = 0;
+		operations[num].rightNumber = 0;
 	}
 
-	operations[num].rightEdge = t - 1;
-	operations[num].rightNumber = right * rightminus;
-	operations[num].rightminus = rightminus;
 
 	if (arr[index] == '+')
-		temp = add(operations[num].leftNumber, operations[num].rightNumber);
+		temp = (operations[num].leftNumber + operations[num].rightNumber);
 	if (arr[index] == '-')
-		temp = sub(operations[num].leftNumber, operations[num].rightNumber);
+		temp = (operations[num].leftNumber - operations[num].rightNumber);
 	if (arr[index] == '*')
-		temp = mult(operations[num].leftNumber, operations[num].rightNumber);
-	if (arr[index] == '/')
-		temp = divide(operations[num].leftNumber, operations[num].rightNumber);
+		temp = (operations[num].leftNumber * operations[num].rightNumber);
+	if (arr[index] == '/') {
+		if (operations[num].rightNumber == 0) {
+			printf("Undefined");
+			exit(0);
+		}
+		else
+			temp = (operations[num].leftNumber / operations[num].rightNumber);
+	}
+	validposition[index] = 1;
+	mainarr[index] = temp;
 
-	j = 0;
-	int sum = temp;
-	for (i = operations[num].rightEdge; i > operations[num].leftEdge; i--) {
-		if (temp < 0)
-			arr[i] = '-';
-		else arr[i] = ' ';
-	}
-	if (sum == 0) {
-		arr[operations[num].rightEdge] = '0';
-	}
-	else
-	for (i = operations[num].rightEdge; i > operations[num].leftEdge; i--) {
-		arr[i] = abs(sum) % 10 + '0';
-		sum /= 10;
-		if (sum == 0)
-			break;
-	}
+	validposition[operations[num].leftEdge] = 0;
+	validposition[operations[num].rightEdge] = 0;
 
-	return (int)temp;
+	return temp;
 }
 
 int checkIfNotOperation(int pos, int num) {
@@ -190,6 +162,7 @@ void setNumbers() {
 		if (isNumber(i) == 1) {
 			k = i;
 			validposition[k] = 1;
+			j = 0;
 			while (isNumber(i) == 1) { // get the length of the number while tracking forward
 				j++;
 				i++;
@@ -197,7 +170,7 @@ void setNumbers() {
 			int t;
 			for (t = j; t > 0; t--) {
 				int c = arr[t + i - j - 1] - '0';
-				int pow = tenToPower(j - i); // set the right number while tracking backwards
+				float pow = tenToPower(j - t); // set the right number while tracking backwards
 				mainarr[k] += c * pow;
 			}	
 			if (isNumber(i) == -3) {
@@ -213,13 +186,14 @@ void setNumbers() {
 			mainarr[k] *= minus;
 		}	
 	}
+
 }
 
 int main() {
 	int i,j, n;
 	arr = inputText(&n);
 	length = n;
-	operations = (struct operation*)malloc(sizeof(struct operation) * maxOperations);
+	operations = (struct operation*)malloc(sizeof(struct operation) * length);
 	validposition = (int*)malloc(sizeof(int) * length);
 	mainarr = (float*)malloc(sizeof(float) * length);
 	numOfOperations = setOrder(0, n, 0);   //set the order of operations from start to the end of the ecuation
@@ -228,25 +202,29 @@ int main() {
 		mainarr[j] = 0;
 		validposition[j] = 0;
 	}
-	//setNumbers();
-
+	setNumbers();
 
 	printarr(n,arr);
+	
 	if (debug) {
+		for (j = 0; j <= length; j++) {
+			printf("%d", validposition[j]);
+		}
+		printf("\n");
 		for (i = 0; i < numOfOperations; i++)
-			printf(" %d->%d ", operations[i].position, i);
+			printf("pos%d->%d ", operations[i].position, i);
 		printf("\n");
 	}
 	for (i = 0; i < numOfOperations; i++) {
 		int temp = calculate(operations[i].position, i);
 		if (debug) {
-			printf("for operaition %d result is %d \n", i, temp);
-			printarr(n, arr);
+			printf("for operation %d result is %d \n", i, temp);
 		}
 		if (i == numOfOperations - 1) {
 			printf("result %d", temp);
 		}
 	}
+	
 	free(validposition);
 	free(mainarr);
 	free(arr);
--- operations.h
+++ operations.h
@@ -3,31 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-float add(float left, float right) {
-	return left + right;
-}
-float sub(float left, float right) {
-	return left - right;
-}
-float mult(float left, float right) {
-	return left * right;
-}
-float divide(float left, float right) {
-	return left / right;
-}
-
-int tenToPower(int n) {     //function that returns the 10^n
-	int i, temp = 1;
-	for (i = 0; i < n; i++) {
-		temp *= 10;
+float tenToPower(int n) {     //function that returns the 10^n
+	float i, temp = 1;
+	if (n < 0) {
+		for (i = 0; i < abs(n); i++)
+			temp /= 10;
 	}
+	else
+		for (i = 0; i < n; i++) {
+			temp *= 10;
+		}
 	return temp;
 }
 
 void printarr(int n, char* arr) {  // prints out the contents of the array for debugging purposes
 	int j;
-	for (j = 0; j <= n; j++)
+	for (j = 0; j < n; j++)
 		printf("%c", arr[j]);
 	printf("\n");
 }
